573d9f30c7124143e00b7cc8ed8138fcb370d1e4
MaximeC64 <maxime.cabanel@hotmail.fr>
2019-09-30 16:28:42 +0200

    Ajut des entities et views - 1ere partie

---
+++ Resotel/Resotel/Entities/Invoice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resotel.Entities
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public float PriceHT { get; set; }
+        public float PriceTTC { get; set; }
+        public string BillingAddress { get; set; }
+        public string State { get; set; }
+        public string Date { get; set; }
+        public Payment Payment { get; set; }
+    }
+}
---
+++ Resotel/Resotel/Entities/Reservation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resotel.Entities
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string DateStart { get; set; }
+        public string DateEnd { get; set; }
+        public Customer Customer { get; set; }
+        public Invoice Invoice { get; set; }
+    }
+}
